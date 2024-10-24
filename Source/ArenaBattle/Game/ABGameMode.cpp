46eecb47513db1319effd3c1a07c36a983711bb0
leeky601 <leeky601@naver.com>
2024-10-24 16:49:12 +0900

    init

---
+++ Source/ArenaBattle/Game/ABGameMode.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/ABGameMode.h"
+#include "Player/ABPlayerController.h"
+
+AABGameMode::AABGameMode()
+{
+	/*static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	if (ThirdPersonClassRef.Class)
+	{
+		DefaultPawnClass = ThirdPersonClassRef.Class;
+	}*/
+
+	static ConstructorHelpers::FClassFinder<APawn> DefaultPawnClassRef(TEXT("/Script/ArenaBattle.ABCharacterPlayer"));
+	if (DefaultPawnClassRef.Class)
+	{
+		DefaultPawnClass = DefaultPawnClassRef.Class;
+	}
+
+	PlayerControllerClass = AABPlayerController::StaticClass();
+}
