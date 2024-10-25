5ae4b8a2c08520aedde524bcfe5d90508a183fc9
leeky601 <leeky601@naver.com>
2024-10-25 19:58:33 +0900

    anim

--- Source/ArenaBattle/Animation/ABAnimInstance.h
+++ Source/ArenaBattle/Animation/ABAnimInstance.h
@@ -3,13 +3,49 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ABAnimInstance.generated.h"
 
 /**
  * 
  */
-class ARENABATTLE_API ABAnimInstance
+UCLASS()
+class ARENABATTLE_API UABAnimInstance : public UAnimInstance
 {
+	GENERATED_BODY()
 public:
-	ABAnimInstance();
-	~ABAnimInstance();
+	UABAnimInstance();
+
+protected:
+	virtual void NativeInitializeAnimation() override;
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
+	TObjectPtr<class ACharacter> Owner;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
+	TObjectPtr<class UCharacterMovementComponent> Movement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	FVector Velocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float GroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float MovingThreshould;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float JumpingThreshould;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsIdle : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsFalling : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsJumping : 1;
+
 };
